b5f67cf38222154525af673a8a86df4aee67fb26
Sharon Lin <sharon.lin89@hotmail.com>
2021-05-02 01:44:24 -0400

    Added slash commands for in-channel configuration

--- README.md
+++ README.md
@@ -7,9 +7,6 @@ The OG PhotoStash app no longer works on Slack, so I'm going to make a new one!!
 ```
 SLACK_TOKEN=<get from Slack API webpage>
 SIGNING_SECRET=<get from Slack API webpage>
-
-# GDrive folder URL: https://drive.google.com/drive/u/0/folders/<TARGET_FOLDER_ID>
-TARGET_FOLDER_ID=<get from Gdrive URL>
 ```
 
 ## Directory
@@ -32,6 +29,7 @@ Make sure to add the Slackbot to your channel, and that should be it!
 ## Running the Server
 Using ngrok.exe, run the command: ```ngrok http 5000```
 - Make sure that you update **Event Subscriptions** on the Slack API with server endpoint (to be edited)
+- Make sure that you update **Slash Commands** on the Slack API with server endpoint (to be edited)
 
 ## Creating GCP Project
 [Follow This Link](https://www.youtube.com/watch?v=6bzzpda63H0)
--- src/bot.py
+++ src/bot.py
@@ -2,12 +2,13 @@ import os
 import slack
 import requests
 import datetime
+import yaml
 from GoogleAPI import Create_Service
 from googleapiclient.http import MediaFileUpload
 from slackeventsapi import SlackEventAdapter
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, Response
 
 
 """
@@ -46,9 +47,29 @@ slack_event_adapter = SlackEventAdapter(
 )
 BOT_ID = client.api_call("auth.test")["user_id"]
 ACCEPTED_FILE_TYPES = ["heic", "heif", "jpeg", "jpg", "mov", "mp4", "mpg", "png", "raw"]
+FOLDER_YAML = "folders.yaml"
 
 
-def download_image(media_url, file_name, file_type):
+def get_folder_id(channel_id):
+    """
+    Given the channel id, return the associated folder ID
+    """
+
+    yaml_file = None
+
+    # If the file does not exist, make one
+    if not os.path.exists(FOLDER_YAML):
+        open(FOLDER_YAML, 'w').close()
+
+    # Load yaml file with contents
+    with open(FOLDER_YAML, "r") as stream:
+        yaml_file = yaml.safe_load(stream) or {}
+    folder_id = yaml_file.get(channel_id, None)
+
+    return folder_id
+
+
+def upload_image(media_url, file_name, file_type, folder_id):
     """
     Given the media URL, upload media to GDrive
 
@@ -66,9 +87,6 @@ def download_image(media_url, file_name, file_type):
     bool
         True on success, False on failure
     """
-    # If the Google service is down, then we are screwed
-    if not GOOGLE_SERVICE:
-        return False
 
     # Make GET request to retrieve media file from Slack server
     media_data = requests.get(
@@ -77,7 +95,7 @@ def download_image(media_url, file_name, file_type):
 
     # If not status OK, return immediately
     if media_data.status_code != 200:
-        return False
+        return False, "Could not retrieve file from Google Drive API."
 
     # Open file content and configure file names
     media = media_data.content
@@ -95,7 +113,7 @@ def download_image(media_url, file_name, file_type):
     # Set up metadata to upload to GDrive
     file_metadata = {
         "name": proper_file_name,
-        "parents": [os.environ["TARGET_FOLDER_ID"]],
+        "parents": [folder_id],
     }
 
     # Perform the upload
@@ -105,19 +123,29 @@ def download_image(media_url, file_name, file_type):
             body=file_metadata, media_body=media_body, fields="id"
         ).execute()
         media_body = None  # NOTE: set to None so that we can delete file
-    except Exception as err:
-        return False
+    except Exception:
+        media_body = None  # NOTE: set to None so that we can delete file
+        try:
+            os.remove(local_file_name)
+        except:
+            pass
+
+        return (
+            False,
+            "Could not upload to Google Drive. Make sure that the Google Drive folder ID is correct "
+            "by running the slash command: `/current-folder-id`. If the folder ID is correct, "
+            "consider changing viewing permissions on your folder (set to the *entire organization*) "
+            "to allow the bot write access.",
+        )
 
     # Now, delete the file (since we don't need a local copy)
     try:
         os.remove(local_file_name)
-    except PermissionError as err:
-        print(f"Failed to delete local file. Looks like a permision error: {err}")
-    except Exception as err:
-        print(f"Failed to delete local file.")
+    except:
+        pass
 
     # Make sure to return true
-    return True
+    return True, None
 
 
 # Keep track of ts to avoid duplicate messages
@@ -142,56 +170,150 @@ def handle_incoming_message(payload):
     # Get relevant slack bot information
     event = payload.get("event", {})
     channel_id = event.get("channel")
+    folder_id = get_folder_id(channel_id)
     user_id = event.get("user")
     ts = event.get("ts")
-    thread_ts = event.get("thread_ts", ts) # for media in threads
+    thread_ts = event.get("thread_ts", ts)  # for media in threads
     proper_date = datetime.datetime.fromtimestamp(float(ts)).strftime(
         "%Y-%m-%d--%H-%M-%S"
     )
 
-    # Make sure it isn't the bot that is sending the message
-    if user_id != BOT_ID and ts not in stored_timestamps:
-        # NOTE: For some reason, Slack API might send multiple requests
-        stored_timestamps.add(ts)
+    # First, make sure that GOOGLE_SERVICE is working
+    if user_id != BOT_ID:
+        if not GOOGLE_SERVICE:
+            client.chat_postMessage(
+                channel=channel_id,
+                text=":x: Failed to upload files.\n\nCould not generate a token.",
+                thread_ts=thread_ts,
+            )
 
-        # Check to see if files is part of the payload
-        if "files" in event:
-            success = 0
-            failure = 0
+            return  # Return immediately
 
-            # Iterate through all file objects
-            for single_file in event["files"]:
-                file_type = single_file["filetype"]
+        # Make sure it isn't the bot that is sending the message
+        if ts not in stored_timestamps:
+            # NOTE: For some reason, Slack API might send multiple requests
+            stored_timestamps.add(ts)
+            encountered_error_messages = set()
 
-                # If the file type is valid, upload to GDrive
-                if file_type in ACCEPTED_FILE_TYPES:
-                    private_url = single_file["url_private"]
-                    file_name = proper_date + " " + single_file["id"]
-                    response = download_image(private_url, file_name, file_type)
+            # Check to see if files is part of the payload
+            if "files" in event:
+                success = 0
+                failure = 0
 
-                    # Tally based on the response
-                    if response:
-                        success += 1
-                    else:
-                        failure += 1
+                # Iterate through all file objects
+                for single_file in event["files"]:
+                    file_type = single_file["filetype"]
 
-            # Construct text message
-            text = ""
-            if success > 0:
-                text += f":white_check_mark: Successfully uploaded {success} "
-                text += "files.\n" if success != 1 else "file.\n"
-            if failure > 0:
-                text += f":x: Failed to upload {failure} "
-                text += "files.\n" if failure != 1 else "file.\n"
+                    # If the file type is valid, upload to GDrive
+                    if file_type in ACCEPTED_FILE_TYPES:
 
-            # Post message to channel
-            if text:
-                client.chat_postMessage(
-                    channel=channel_id,
-                    text=text,
-                    thread_ts=thread_ts,
-                )
+                        # Next, get the folder ID for this channel
+                        if not folder_id:
+                            client.chat_postMessage(
+                                channel=channel_id,
+                                text=":x: Failed to upload files.\n\nThe Google Drive folder ID is not configured "
+                                "for this channel. Use the slash command: `/current-folder-id` to check the "
+                                "current Google Drive folder ID.",
+                                thread_ts=thread_ts,
+                            )
+                            return  # Return immediately
+
+                        private_url = single_file["url_private"]
+                        file_name = proper_date + " " + single_file["id"]
+                        response, message = upload_image(
+                            private_url, file_name, file_type, folder_id
+                        )
+
+                        # Tally based on the response
+                        if response:
+                            success += 1
+                        else:
+                            failure += 1
+                            encountered_error_messages.add(" • " + message)
+
+                # Construct text message
+                text = ""
+                if success > 0:
+                    text += f":white_check_mark: Successfully uploaded {success} "
+                    text += "files.\n" if success != 1 else "file.\n"
+                if failure > 0:
+                    text += f":x: Failed to upload {failure} "
+                    text += "files.\n\n" if failure != 1 else "file.\n\n"
+                    text += "Errors:\n" + "\n".join(encountered_error_messages)
+
+                # Post message to channel
+                if text:
+                    client.chat_postMessage(
+                        channel=channel_id,
+                        text=text,
+                        thread_ts=thread_ts,
+                    )
+
+
+@app.route("/config-folder-id", methods=["POST"])
+def handle_folder_config():
+    """
+    Handle slack command for configuring GDrive folder ID
+    """
+    # Retrieve channel and folder ID's from POST request
+    data = request.form
+    channel_id = data["channel_id"]
+    folder_id = data["text"]
+
+    # Load the YAML file of folder mappings
+    yaml_file = None
+
+    if not os.path.exists(FOLDER_YAML):
+        open(FOLDER_YAML, 'w').close()
+
+    with open(FOLDER_YAML, "r") as stream:
+        yaml_file = yaml.safe_load(stream) or {}
+
+    # Create Slack Bot reply message
+    message = ""
+
+    if len(folder_id.split()) > 1:  # Make sure it is one word
+        message = "The Google Drive folder ID should be one string. Please verify and try again."
+    else:  # Otherwise, we can update the yaml file
+        yaml_file.update({channel_id: folder_id})
+
+        with open(FOLDER_YAML, "w") as yamlfile:
+            yaml.safe_dump(yaml_file, yamlfile)
+        message = (
+            f"The Google Drive folder ID for this channel is set to: "
+            f"<https://drive.google.com/drive/u/0/folders/{folder_id}|{folder_id}>"
+        )
+
+    # Return a message with the current channel_id
+    client.chat_postMessage(channel=channel_id, text=message)
+
+    return Response(), 200
+
+
+@app.route("/current-folder-id", methods=["POST"])
+def return_current_folder():
+    """
+    Given the channel ID, return the GDrive folder ID from FOLDER_YAML
+    """
+    data = request.form
+    channel_id = data["channel_id"]
+    folder_id = get_folder_id(channel_id)
+
+    # Print message based on the folder ID
+    message = ""
+    if not folder_id:
+        message = (
+            "This channel's Google Drive folder ID has *not* been configured. "
+            "Use the slash command: `/config-folder-id <folder-id>` to configure this channel's Google Drive folder ID."
+        )
+    else:
+        message = f"This channel's Google Drive folder ID is: <https://drive.google.com/drive/u/0/folders/{folder_id}|{folder_id}>"
+
+    # Return a message with the current channel_id
+    client.chat_postMessage(channel=channel_id, text=message)
+
+    return Response(), 200
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
